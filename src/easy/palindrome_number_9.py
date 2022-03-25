ce16e9fe5a80b89c80184db9a2521b500f4646a0
Ahmet Y. Cengiz <ahmet9cengiz@gmail.com>
2022-03-25 12:41:33 -0400

    9 & 14

---
+++ src/easy/palindrome_number_9.py
@@ -0,0 +1,20 @@
+class Solution(object):
+
+    # Time Complexity: O(n)
+    @staticmethod
+    def palindrome_number(x):
+        if x < 0:
+            return False
+
+        x = str(x)
+        left_index = 0
+        right_index = len(x) - 1
+
+        while left_index > right_index:
+            if x[left_index] != x[right_index]:
+                return False
+            else:
+                left_index += 1
+                right_index -= 1
+
+        return True
