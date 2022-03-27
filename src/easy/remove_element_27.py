e5492c90132124e9ef690ce5e1ce32b5a7497124
Ahmet Y. Cengiz <ahmet9cengiz@gmail.com>
2022-03-27 10:44:01 -0400

    q27

---
+++ src/easy/remove_element_27.py
@@ -0,0 +1,26 @@
+class Solution(object):
+
+    # Time Complexity: O(n)
+    @staticmethod
+    def remove_element(nums, val):
+        size = len(nums)
+        j = size -1  # going backwards 
+        i = 0
+
+        while i <= j:
+            if nums[i] == val:
+                if nums[j] != val:
+                    nums[i] = nums[j]
+                    j -= 1
+                    i += 1
+                else:  
+                    j -= 1
+            else:
+                i += 1
+
+        return j+1
+
+if __name__ == '__main__':
+    s = Solution()
+    val = 2
+    nums = [0, 1, 2, 2, 3, 0, 4, 2]
